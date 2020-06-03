bfb9bc22af12cc65071686e5541ac28f6994256a
Aayush Rajput <37001634+aayushhh@users.noreply.github.com>
2020-06-04 02:23:37 +0530

    Building different test cases

--- calculator.java
+++ calculator.java
@@ -4,17 +4,31 @@ import ch.lamdaj.function.convert.Converter;
 import static ch.lamdaj.Lamda.*;
 
 public class Calculator{
-if(text.isEmpty()){
-return 0;
-
-}else if (text.contains(",")){
+  if(text.isEmpty()){
+    return 0;
+    }
+  
+  else if (text.contains(",")) {
+    
 String[] tokens = text.split(",");
+    
   List<Integer> numbers = convert(tokens, toInt());
-return Integer.parse(tokens[0])+ toInt (tokens[1]);
-}else {
-return toInt(text);
+    
+  return Integer.parse(tokens[0])+ toInt (tokens[1]);
 }
-
+  else {
+    return toInt(text);
+       }
+  
+private static Converter<String, Integer>toInt(){
+  return new Converter<String, Integer>() {
+    
+      public Integer convert(String from){
+        
+          return toInt(from);
+      }
+  };
+}
   
   
 }
